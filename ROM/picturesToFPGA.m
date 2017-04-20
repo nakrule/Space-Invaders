3743d199fa150c081862303a8af92bf62709a1b2
Nakrule <samuelriedo@mordor.sofr.hefr.lan>
2017-04-20 15:58:43 +0200

    new pictures in vhdl
    added win and gameover

--- ROM/picturesToFPGA.m
+++ ROM/picturesToFPGA.m
@@ -1,5 +1,5 @@
 %read the image
-I = imread('pictures/aliens/yellow.jpg');	
+I = imread('pictures/gameover.jpg');	
 [x,y,z] = size(I); % x = width, y = heigh
 width = x-1;
 		
@@ -34,7 +34,7 @@ R = bitshift(R, 5);
 COLOR = R+G+B;
 
 %save variable COLOR to a file in HEX format for the chip to read
-fileID = fopen ('vhdlTable/yellow.vhd', 'w');
+fileID = fopen ('vhdlTable/gameOver.vhd', 'w');
 fprintf (fileID, 'type memoryPicture is array(0 to ');
 fprintf (fileID, '%d', y-1);
 fprintf (fileID, ', 0 to ');
